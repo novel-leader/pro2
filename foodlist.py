53be6ee0428a56cd74a88aad91d86d5548e3b9fe
novel <hoocarol4@gmail.com>
2022-03-06 10:53:27 +0900

    220306-1

--- foodlist.py
+++ foodlist.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 options = webdriver.ChromeOptions()
-options.headless = False
+# options.headless = True
 options.add_argument("window-size=1920x1080")
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36")
 browser = webdriver.Chrome(options=options)
